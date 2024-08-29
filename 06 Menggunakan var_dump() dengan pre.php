1a404dfa9c075627349b6a0dfde967f6887ec8cc
kakakjihan <jihansahfitri6@gmail.com>
2024-08-29 12:14:51 +0700

    membuat var dump dengan pre

--- 06 Menggunakan var_dump() dengan pre.php
+++ 06 Menggunakan var_dump() dengan pre.php
@@ -3,5 +3,5 @@ $variabel_array = array("apel", "pisang", "ceri");
 echo "<pre>";
 var_dump($variabel_array);
 echo "</pre>";
-print_r("<P><strong>by :jihan syafitri");
+print_r("<P><strong>by :jihann syafitri");
 ?>
