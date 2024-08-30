d9fc5955b8ab443952ef32201b73ddb3f45924a0
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 11:04:07 +0700

    menggunakan strcmp&strcasecmp

--- 27 strcmp dan strcasecmp.php
+++ 27 strcmp dan strcasecmp.php
@@ -1,4 +1,4 @@
 <?php
 strcmp("Hello World!", "Hello World!"); // Outputs: 0
 echo strcasecmp("hello world!", "HELLO WORLD!"); echo // Outputs: 0
-print_r("<P><strong>by :jihann syafitrii");
+print_r("<P><strong>by :jihan syafitri");
