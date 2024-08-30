2d97cef679aa7a8a1ab601cf90bb28c31839fd15
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 13:37:49 +0700

    pertanyaan else

--- 33 percabangan if else.php/34 Percabangan If/Elseif/39 pernyataan else.php
+++ 33 percabangan if else.php/34 Percabangan If/Elseif/39 pernyataan else.php
@@ -4,6 +4,6 @@ if ($nilai > 70) {
   echo "Selamat, kamu lulus!";
 } else {
   echo "Maaf, kamu harus mengulang.";
-  print_r("<P><strong>by :jiihann syafitrii");
+  print_r("<P><strong>by :jihann syafitri");
 }
 
