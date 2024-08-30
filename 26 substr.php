afc1a6e51394af7b0dace6eafa781e25f4259431
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 11:03:26 +0700

    membuat substr

--- 26 substr.php
+++ 26 substr.php
@@ -2,4 +2,4 @@
 $text = "Hello World!";
 echo substr($text, 0, 5); // Outputs: "Hello"
 echo substr($text, 6); // Outputs: "World!"
-print_r("<P><strong>by :jihann syafitrii");
+print_r("<P><strong>by :jihan syafitri");
