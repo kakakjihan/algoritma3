dbe25f415372029984f93053821be555a4c1cb4d
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 11:01:25 +0700

    menggunakan trim,ltrim&rtrim

--- 23 trim, ltrim, dan rtrim.php
+++ 23 trim, ltrim, dan rtrim.php
@@ -3,5 +3,5 @@ $text = " Hello World! ";
 echo "[" . trim($text) . "]"; // Outputs: "[Hello World!]"
 echo "[" . ltrim($text) . "]"; // Outputs: "[Hello World! ]"
 echo "[" . rtrim($text) . "]"; // Outputs: "[ Hello World!]"
-print_r("<P><strong>by :jihann syafitrii");
+print_r("<P><strong>by :jiihan syafitrii");
 
