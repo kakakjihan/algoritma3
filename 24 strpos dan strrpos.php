0da8462953ace792e130a4cb52b15a88f231e9da
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 11:02:18 +0700

    menggunakan strpos&strrpos

--- 24 strpos dan strrpos.php
+++ 24 strpos dan strrpos.php
@@ -2,4 +2,4 @@
 $text = "Hello World!";
 echo strpos($text, "World"); // Outputs: 6
 echo strrpos($text, "o"); // Outputs: 7
-print_r("<P><strong>by :jihann syafitrii");
+print_r("<P><strong>by :jihan syafitri");
