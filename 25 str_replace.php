af3bd871cc14785cfc44c852f879ed39edddd266
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 11:02:55 +0700

    menggunakan strreplace

--- 25 str_replace.php
+++ 25 str_replace.php
@@ -1,4 +1,4 @@
 <?php
 $text = "Hello World!";
 echo str_replace("World", "PHP", $text); // Outputs: "Hello PHP!"
-print_r("<P><strong>by :jihann syafitrii");
+print_r("<P><strong>by :jihan syafitri");
