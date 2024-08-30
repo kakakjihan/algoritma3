906d7bba60d57f3a3373375d079c7988b06246a3
kakakjihan <jihansahfitri6@gmail.com>
2024-08-30 13:42:10 +0700

    menggunakan konversi ke boolean

--- 33 percabangan if else.php/34 Percabangan If/Elseif/43 konversi ke boolean.php
+++ 33 percabangan if else.php/34 Percabangan If/Elseif/43 konversi ke boolean.php
@@ -5,6 +5,6 @@ if ($nilai) {
     echo "Variabel ini bernilai true";
 } else {
     echo "Variabel ini bernilai false";
-    print_r("<P><strong>by :jiihann syafitrii");
+    print_r("<P><strong>by :jihann syafitri");
 }
 // Keluaran akan "Variabel ini bernilai false" karena nilai 0 dianggap false.
